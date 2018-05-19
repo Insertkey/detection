73370f0f22c6e89960159738bf129fcd2ac462fb
Insertkey <707607105@qq.com>
2018-05-19 13:01:52 +0800

    TCPserver
    TCP服务端程序

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var net = require('net');
+const net = require('net');
 var mongoose=require('mongoose');
 
-var HOST = '111.230.91.243';
-var PORT = 6969;
+const HOST = '111.230.91.243';
+const PORT = 6969;
 
-data=net.createServer(function(socket) {
+const server=net.createServer(function(socket) {
   //连接成功时显示连接设备的地址和端口号
   console.log('connected: ' +
     socket.remoteAddress + ':' + socket.remotePort);
@@ -14,16 +14,23 @@ data=net.createServer(function(socket) {
     console.log('closed: '+socket.remoteAddress + ' ' + socket.remotePort);
   });
 
-  socket.on('data',function () {
-    var MongoClient=require('mongodb').MongoClient;
-    var url="mongodb://localhost:27017/";
+  socket.setEncoding('utf8');
 
-    console.log(data.toString());
+  socket.on('data',function (data) {
+    var MongoClient=require('mongodb').MongoClient;
+    const url="mongodb://localhost:27017/";
+    console.log(data);
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var dbo = db.db("test");
-      var myobj = data;
-      dbo.collection("detction").insertOne(myobj, function(err, res) {
+      //获取当前时间并转换为字符串
+      var date=new Date();
+      var nowDate=date.toUTCString();
+      //处理字符串
+      var string=data.slice(0,data.length-1)+','+'"date":'+'"'+nowDate+'"'+'}';
+      //将字符串转换为一个JSON对象
+      var myobj = JSON.parse(string);
+      dbo.collection("detection").insertOne(myobj, function(err, res) {
         if (err) throw err;
         console.log("文档插入成功");
       });
