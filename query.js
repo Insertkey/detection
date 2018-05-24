c251ad7de3561b768822f3364f069bb0ffc4b8ad
Insertkey <707607105@qq.com>
2018-05-24 22:18:31 +0800

    2018/5/24
    修改了前端页面，添加了数据库查询功能

--- query.js
+++ query.js
@@ -1,21 +1,31 @@
 var MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
+var fs=require('fs');
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("test");
-  //取得现在时间的毫秒数
-  now = Date.now();
-  //获取前两分钟插入数据库的数据
-  dbo.collection("detection").find(/*{"date":{$gt:now-1000*60*2}}*/).toArray(function (err, result) {
+function getTenData() {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    //取得一个最新的数据
-    data=result.pop();
-    return data;
-  });
-});
+    var dbo = db.db("test");
+    //取得现在时间的毫秒数
+    now = Date.now();
 
+    //获取前两分钟插入数据库的数据
+    dbo.collection("detection").find(/*{"date":{$gt:now-1000*60*2}}*/).toArray(function (err, result) {
+      if (err) throw err;
+      //取得一个最新的数据
+      data = result.slice(result.length - 10, result.length);
+      db.close();
 
+      fs.writeFile('getTenData.txt', JSON.stringify(data), function (err) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("数据写入成功！");
+      });
+    });
+  })
+}
+setInterval(getTenData,5000);
 
 
 
